7211f12d372115523ed1339706916e99e86e4f5f
Luca Stefani <luca020400@lineageos.org>
2017-09-21 22:55:47 +0200

    Let's use reactivex observable

--- cvv/src/main/kotlin/com/luca020400/cvv/API.kt
+++ cvv/src/main/kotlin/com/luca020400/cvv/API.kt
@@ -2,12 +2,11 @@ package com.luca020400.cvv
 
 import com.luca020400.cvv.models.LoginRequest
 import com.luca020400.cvv.models.LoginResponse
-
-import retrofit2.Call
+import io.reactivex.Observable
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface API {
     @POST("rest/v1/auth/login")
-    fun doLogin(@Body loginRequest: LoginRequest): Call<LoginResponse>
+    fun doLogin(@Body loginRequest: LoginRequest): Observable<LoginResponse>
 }
